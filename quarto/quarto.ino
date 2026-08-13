f0d3905d8d2ec7278d28c2d79f1dc8ef0ed25c1e
Steam <steam@poli.br>
2026-08-13 15:55:25 -0300

    Update quarto.ino

--- quarto/quarto.ino
+++ quarto/quarto.ino
@@ -1,1 +1,17 @@
-    hbdysbdy
+const int sensor = 2;
+const int led = 13;
+
+void setup() {
+  pinMode(sensor, INPUT);
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  int presenca = digitalRead(sensor);
+
+  if (presenca == HIGH) {
+    digitalWrite(led, HIGH);  // Liga o LED
+  } else {
+    digitalWrite(led, LOW);   // Desliga o LED
+  }
+}
